e731867195fbc2fc23ac9e4f5fdda5ef9d0a13d1
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-23 15:24:51 +0800

    update functions

--- feedbackapp/src/components/Feedback.js
+++ feedbackapp/src/components/Feedback.js
@@ -1,11 +1,21 @@
+import PropTypes from "prop-types";
 import Card from "./shared/Card";
-function Feedback({ item }) {
+import { FaTimes } from "react-icons/fa";
+function Feedback({ item, handleDelete }) {
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => handleDelete(item.id)}>
+        <FaTimes />
+      </button>
       <div className="text display">{item.text}</div>
     </Card>
   );
 }
+
+Feedback.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default Feedback;
