faecac1ba1f14192e314a409a9d7adcddd1404b1
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-23 15:24:29 +0800

    update functions

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -5,12 +5,17 @@ import FeedbackData from "./data/feedBackData";
 
 export const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if(window.confirm("Are you sure you want to delete?")){
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
-          </div>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+      </div>
     </>
   );
 };
