114339051525e9f36535eca76b959779ae28b3be
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-23 15:25:08 +0800

    update functions

--- feedbackapp/src/components/Feedbacklist.js
+++ feedbackapp/src/components/Feedbacklist.js
@@ -1,19 +1,30 @@
+import PropTypes from 'prop-types'
 import Feedback from "./Feedback";
 import Card from "./shared/Card";
 
 
-function Feedbacklist({ feedback }) {
-  if (!feedback || feedback.lenght === 0) {
+function Feedbacklist({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
     return <h1>No feedback yet</h1>;
   }
   return(
     <div className="feedback-list">
       {feedback.map((item) => 
-        <Feedback key={item.id} item={item} />
+        <Feedback key={item.id} item={item} handleDelete={handleDelete}/>
       )}
-      <Card>Hello</Card>
     </div>
   );
+}
+
+Feedbacklist.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    }
+    )
+  )
 }
 
 export default Feedbacklist;
