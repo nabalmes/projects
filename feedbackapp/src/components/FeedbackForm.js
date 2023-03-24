153107dbd793251a26d0bdab3e4369bc42c888f3
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-24 18:16:41 +0800

    update functionality

--- feedbackapp/src/components/FeedbackForm.js
+++ feedbackapp/src/components/FeedbackForm.js
@@ -1,38 +1,55 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState("");
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState('')
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
-  
   const handleTextChange = (e) => {
-    if(text === '') {
-        setBtnDisabled(true)
-        setText(null)
-    }else if(text !== '' && text.trim().length <= 10) {
-        setBtnDisabled(true)
-        setMessage('Putanginaka ka kulang ang nilagay mo!')
-    }else {
-        setMessage(null)
-        setBtnDisabled(false)
+    if (text === "") {
+      setBtnDisabled(true);
+      setText(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Putanginaka kulang ang nilagay mo!");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
     setText(e.target.value);
   };
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating
+      };
+      handleAdd(newFeedback)
+      setText('')
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
             type="text"
-            placeholder="Write review" value={text}
+            placeholder="Write review"
+            value={text}
           />
-          <Button type="submit" isDisabled={btnDisabled}>Submit</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Submit
+          </Button>
         </div>
 
         {message && <div className="message">{message}</div>}
