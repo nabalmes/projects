4d9e2b53d80e02f5f2af07f9cf62dfcfeb5da161
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-24 18:16:13 +0800

    update functionality

--- feedbackapp/src/components/Feedbacklist.js
+++ feedbackapp/src/components/Feedbacklist.js
@@ -1,30 +1,31 @@
-import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from "framer-motion";
+import PropTypes from "prop-types";
 import Feedback from "./Feedback";
-import Card from "./shared/Card";
-
 
 function Feedbacklist({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <h1>No feedback yet</h1>;
   }
-  return(
+  return (
     <div className="feedback-list">
-      {feedback.map((item) => 
-        <Feedback key={item.id} item={item} handleDelete={handleDelete}/>
-      )}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Feedback key={item.id} item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
 
 Feedbacklist.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    }
-    )
-  )
-}
+  feedback: PropTypes.array,
+};
 
 export default Feedbacklist;
