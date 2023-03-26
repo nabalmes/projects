606e0fa462611ac5833ca9816484eadda66dbed6
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-26 15:03:16 +0800

    fix error

--- feedbackapp/src/index.js
+++ feedbackapp/src/index.js
@@ -1,11 +1,8 @@
 import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import { App } from "./App";
 import './index.css'
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
