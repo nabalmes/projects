1f6f399a755eac0ceeeed596ed9d49a0c519787f
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-27 15:13:46 +0800

    fix functionality

--- search-data/src/components/Search.js
+++ search-data/src/components/Search.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Search({ setSearch }) {
   return (
     <div className="search-container">
@@ -14,17 +13,3 @@ function Search({ setSearch }) {
 }
 
 export default Search;
-=======
-
-function Search({setSearch}) {
-
-  return (
-    <div className="search-container">
-        <h1>Search Country</h1>
-        <input onChange={(e) => setSearch(e.target.value)} type="text" name="search" placeholder="search name" />
-    </div>
-  )
-}
-
-export default Search
->>>>>>> d29b9bcf55f99f28539e92899e24afa41a7d46a6
