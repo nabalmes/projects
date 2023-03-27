b4925bc8acc4386e826043e81b0cc62679ea6a35
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-27 15:14:05 +0800

    fix functionality

--- search-data/src/components/Table.js
+++ search-data/src/components/Table.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-function Table({ filteredData }) {
+function Table({ filteredData, search }) {
   return (
     <>
-=======
-import { useState } from "react";
-import { Data } from "../data/Data";
-import Search from "./Search";
-console.log(Data);
-function Table() {
-    const [search, setSearch] = useState('')
-  return (
-    <>
-      <Search setSearch={setSearch}/>
->>>>>>> d29b9bcf55f99f28539e92899e24afa41a7d46a6
       <table>
         <thead>
           <tr>
@@ -23,17 +11,13 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-<<<<<<< HEAD
-          {filteredData.map((item) => (
-=======
-          {Data
+          {filteredData
           .filter((item) => {
-            return search.toLowerCase() === ""
+            return search === ''
             ? item 
-            : item.first_name.toLowerCase().includes(search)
+            : item.first_name.toLowerCase().includes(search) || item.last_name.toLowerCase().includes(search)
           })
           .map((item) => (
->>>>>>> d29b9bcf55f99f28539e92899e24afa41a7d46a6
             <tr key={item.id}>
               <td>{item.first_name}</td>
               <td>{item.last_name}</td>
