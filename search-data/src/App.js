44efa5917fa03cbe360a0fa6827f044117a3d8d6
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-27 15:11:37 +0800

    fix functionality

--- search-data/src/App.js
+++ search-data/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import Table from "./components/Table";
 import Search from "./components/Search";
@@ -7,27 +6,18 @@ import "./App.css";
 
 function App() {
   const [search, setSearch] = useState("");
-  const searchBy = (data) => {
-    return data.filter(
-      (item) =>
-        item.first_name.toLowerCase().includes(search) ||
-        item.last_name.toLowerCase().includes(search) ||
-        item.email.toLowerCase().includes(search)
-    );
-  };
+  // const searchBy = (data) => {
+  //   return data.filter(
+  //     (item) =>
+  //       item.first_name.toLowerCase().includes(search) ||
+  //       item.last_name.toLowerCase().includes(search) ||
+  //       item.email.toLowerCase().includes(search)
+  //   );
+  // };
   return (
     <div className="App">
       <Search setSearch={setSearch} />
-      <Table filteredData={searchBy(Data)} />
-=======
-import Table from './components/Table';
-import './App.css';
-
-function App(search) {
-  return (
-    <div className="App">
-      <Table/>
->>>>>>> d29b9bcf55f99f28539e92899e24afa41a7d46a6
+      <Table filteredData={Data} search={search}/>
     </div>
   );
 }
