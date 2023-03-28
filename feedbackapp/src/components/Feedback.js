4a9db4334b5999227a2c83bb68aa16fa18bcbdd9
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-28 11:16:44 +0800

    added update function

--- feedbackapp/src/components/Feedback.js
+++ feedbackapp/src/components/Feedback.js
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
-import { FaTimes } from "react-icons/fa";
-function Feedback({ item, handleDelete }) {
+import { FaTimes, FaEdit } from "react-icons/fa";
+function Feedback({ item, handleDelete, handleEdit }) {
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close" onClick={() => handleDelete(item.id)}>
         <FaTimes />
+      </button>
+      <button className="edit" onClick={() => handleEdit(item)}>
+        <FaEdit color="purple"/>
       </button>
       <div className="text display">{item.text}</div>
     </Card>
