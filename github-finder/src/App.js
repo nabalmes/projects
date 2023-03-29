ce2fd64707c9e2fa4b40c40bfd84bdae7e5af634
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-29 16:34:12 +0800

    github finder

---
+++ github-finder/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-purple-500">
+       <h1 className='text-xl'>Hello Bitch!</h1>
+       <button className="btn">Submit</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
