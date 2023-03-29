ad31f0d4b45ae829a9a0683ad4ed4aab7ebf2671
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-29 13:46:41 +0800

    added add edit delete function

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -1,4 +1,3 @@
-import {v4 as uuidv4} from 'uuid'
 import { useEffect, useState } from "react"; 
 import Header from "./components/Header";
 import FeedbackList from "./components/Feedbacklist";
@@ -18,20 +17,29 @@ export const App = () => {
 
   //fetch data from backend
   const fetchFeedback = async() => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
   }
 
   //ADD Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(`/feedback`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newFeedback)
+      })
+    
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
   //Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if(window.confirm("Are you sure you want to delete?")){
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
