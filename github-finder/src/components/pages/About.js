d5a7baf9ff0ddefe0faca0bb35b51c67f7dce064
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-30 14:32:25 +0800

    added pages

---
+++ github-finder/src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className="mb-4 text-2xl font-light">A React app to search Github 
+        profiles and see profile details.
+        </p>
+        <p className="text-lg text-gray-400">
+            Layout By:
+            <a href="https://nab2023.vercel.app" className="text-white">
+                Neil Adrian Balmes
+            </a>
+        </p>
+    </div>
+  )
+}
+
+export default About
