eeaac07b533d3e56c854393dc495d085b9c4e51f
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-05 00:27:36 +0800

    added new function

--- github-finder/src/components/pages/Home.js
+++ github-finder/src/components/pages/Home.js
@@ -1,14 +1,12 @@
-import UserResults from "../users/UserResults"
+import UserResults from "../users/UserResults";
 
 function Home() {
   return (
     <>
-        <UserResults />
-        <div>{process.env.REACT_APP_API_URL}</div>
+      <UserResults />
+      <div>{process.env.REACT_APP_API_URL}</div>
     </>
-    
-  )
+  );
 }
 
-
-export default Home
+export default Home;
