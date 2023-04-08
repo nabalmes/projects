6f1d6ca04a5faf4df125b9114703ba97f3b3660f
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-08 19:10:52 +0800

    remove headers to fix error fetching data

--- github-finder/src/components/users/UserResults.js
+++ github-finder/src/components/users/UserResults.js
@@ -10,12 +10,7 @@ function UserResults() {
 
   const request = async () => {
     const response = await fetch(`${process.env.
-      REACT_APP_API_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.
-          REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+      REACT_APP_API_URL}/users`);
 
     const data = await response.json();
     console.log(data);
