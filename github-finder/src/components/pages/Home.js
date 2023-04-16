c2f766ace2329c5c4a64749d17ee683fbe8f6577
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-16 14:24:10 +0800

    remove console logs

--- github-finder/src/components/pages/Home.js
+++ github-finder/src/components/pages/Home.js
@@ -3,8 +3,6 @@ import UserResults from "../users/UserResults";
 function Home() {
   return (
     <>
-    {process.env.REACT_APP_GITHUB_TOKEN}
-    {process.env.REACT_APP_API_URL}
       <UserResults />
     </>
   );
