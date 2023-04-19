532ecd1266926383aa837e05a53c8b529000e121
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-19 20:17:03 +0800

    fixed error

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { createContext, useState } from "react";
 
-function GithubContext() {
-  return (
-    <div>GithubContext</div>
-  )
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({children}) => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const request = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/users`);
+    
+        const data = await response.json();
+        setUsers(data);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 3000);
+      };
 }
-
-export default GithubContext
