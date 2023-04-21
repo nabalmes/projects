ee2e69df82c696884d2e286590db110b6996a78d
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-21 18:37:38 +0800

    fix bugs

--- github-finder/src/components/users/UserResults.js
+++ github-finder/src/components/users/UserResults.js
@@ -2,22 +2,9 @@ import { useEffect, useState } from "react";
 import UserItem from "./UserItem";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
     request();
   }, []);
-
-  const request = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/users`);
-
-    const data = await response.json();
-    setUsers(data);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  };
 
   if (!isLoading) {
     return (
