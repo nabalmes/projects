e626af0a8ab3fcf21b6ad91fa8be5e62eb6dc94b
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-23 00:31:53 +0800

    optimized code

--- github-finder/src/components/layout/Footer.js
+++ github-finder/src/components/layout/Footer.js
@@ -1,16 +1,15 @@
-import { FaSlackHash } from 'react-icons/fa'
+import { FaSlackHash } from "react-icons/fa";
 
 function Footer() {
-    const footerYear = new Date().getFullYear()
+  const footerYear = new Date().getFullYear();
   return (
     <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
-        <div>
-            <FaSlackHash className='text-xl'/>
-            <p>Copyrights &copy; {footerYear} ALL RIGHTS RESERVED</p>
-        </div>
+      <div>
+        <FaSlackHash className="text-xl" />
+        <p>Copyrights &copy; {footerYear} ALL RIGHTS RESERVED</p>
+      </div>
     </footer>
   );
 }
-
 
 export default Footer;
