fc23ea31c310e6b35895ca2402d16dfc9fa06bd9
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-24 21:10:30 +0800

    added timeout on loader

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -2,19 +2,21 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_API_URL
+const GITHUB_URL = process.env.REACT_APP_API_URL;
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const request = async () => {
-    console.log(GITHUB_URL)
+    console.log(GITHUB_URL);
     const response = await fetch(`${GITHUB_URL}/users`);
 
     const data = await response.json();
     setUsers(data);
-    setIsLoading(false);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   };
 
   return (
