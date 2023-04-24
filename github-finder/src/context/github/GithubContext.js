a72649f104b072dad18c664db8f9619a8a55eddd
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-24 20:58:32 +0800

    fixed bugs

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -1,31 +1,32 @@
 import { createContext, useState } from "react";
 
-const GithubContext = createContext()
+const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
-export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+export const GithubProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const request = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`);
 
-    const request = async () => {
-        const response = await fetch(`${process.env.GITHUB_URL}/users`);
-    
-        const data = await response.json();
-        setUsers(data);
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 3000);
-      };
+    const data = await response.json();
+    setUsers(data);
+    setIsLoading(false);
+  };
 
-      return <GithubContext.Provider value={({
+  return (
+    <GithubContext.Provider
+      value={{
         users,
-        isLoading
-      })
-      }>
-        {children}
-      </GithubContext.Provider>
-}
+        isLoading,
+        request,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
 export default GithubContext;
