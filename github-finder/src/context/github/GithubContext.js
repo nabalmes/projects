25ef4bdc835d6d629f43cae3bbeaf80c766cb65b
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-04-24 21:03:41 +0800

    fixed bugs on api

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -2,13 +2,14 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_URL = process.env.REACT_APP_API_URL
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const request = async () => {
+    console.log(GITHUB_URL)
     const response = await fetch(`${GITHUB_URL}/users`);
 
     const data = await response.json();
